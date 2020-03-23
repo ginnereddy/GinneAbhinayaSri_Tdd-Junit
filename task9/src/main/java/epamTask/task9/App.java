37a7f84407e1f302fd472ada194cad55812ea8d7
Ginne Abhinaya Sri <60144898+ginnereddy@users.noreply.github.com>
2020-03-23 09:40:27 -0400

    Add files via upload

---
+++ task9/src/main/java/epamTask/task9/App.java
@@ -0,0 +1,13 @@
+package epamTask.task9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
